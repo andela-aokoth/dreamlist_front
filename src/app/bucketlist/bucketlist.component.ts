3b7f1d09b090a9d80de4d9ce07fb20c75f080da9
Arnold Okoth <arnold.okoth@andela.com>
2016-12-20 12:55:14 +0300

    Add bucketlist skeleton w/ dummy data

--- src/app/bucketlist/bucketlist.component.ts
+++ src/app/bucketlist/bucketlist.component.ts
@@ -6,7 +6,128 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bucketlist.component.css']
 })
 export class BucketlistComponent implements OnInit {
-
+  bucketlists: Array<any> = [
+    {
+      "bucketlist_id": 1,
+      "name": "Pluralsight Learning Paths",
+      "date_created": "12th December 2015",
+      "date_modified": "12th December 2015",
+      "description": "Getting the best out of my Pluralsight subscription",
+      "items": [
+        {
+          "item_id": 1,
+          "name": "JavaScript",
+          "date_created": "12th December 2015",
+          "date_modified": "12th December 2015",
+          "done": false
+        },
+        {
+          "item_id": 2,
+          "name": "AngularJS",
+          "date_created": "12th December 2015",
+          "date_modified": "12th December 2015",
+          "done": false
+        },
+        {
+          "item_id": 3,
+          "name": "Python",
+          "date_created": "12th December 2015",
+          "date_modified": "12th December 2015",
+          "done": false
+        }
+      ]
+    },
+    {
+      "bucketlist_id": 2,
+      "name": "Before Turning Twenty Five",
+      "date_created": "12th December 2015",
+      "date_modified": "12th December 2015",
+      "description": "Getting the best out of my Pluralsight subscription",
+      "items": [
+        {
+          "item_id": 1,
+          "name": "Tomorrowland",
+          "date_created": "12th December 2015",
+          "date_modified": "12th December 2015",
+          "done": false
+        },
+        {
+          "item_id": 2,
+          "name": "Google I/O",
+          "date_created": "12th December 2015",
+          "date_modified": "12th December 2015",
+          "done": false
+        },
+        {
+          "item_id": 3,
+          "name": "WWDC",
+          "date_created": "12th December 2015",
+          "date_modified": "12th December 2015",
+          "done": false
+        }
+      ]
+    },
+    {
+      "bucketlist_id": 3,
+      "name": "Before Turning Scary Thirty",
+      "date_created": "12th December 2015",
+      "date_modified": "12th December 2015",
+      "description": "Getting the best out of my Pluralsight subscription",
+      "items": [
+        {
+          "item_id": 1,
+          "name": "Tomorrowland",
+          "date_created": "12th December 2015",
+          "date_modified": "12th December 2015",
+          "done": false
+        },
+        {
+          "item_id": 2,
+          "name": "Google I/O",
+          "date_created": "12th December 2015",
+          "date_modified": "12th December 2015",
+          "done": false
+        },
+        {
+          "item_id": 3,
+          "name": "WWDC",
+          "date_created": "12th December 2015",
+          "date_modified": "12th December 2015",
+          "done": false
+        }
+      ]
+    },
+    {
+      "bucketlist_id": 4,
+      "name": "Before Turning Thirty Five",
+      "date_created": "12th December 2015",
+      "date_modified": "12th December 2015",
+      "description": "Getting the best out of my Pluralsight subscription",
+      "items": [
+        {
+          "item_id": 1,
+          "name": "Tomorrowland",
+          "date_created": "12th December 2015",
+          "date_modified": "12th December 2015",
+          "done": false
+        },
+        {
+          "item_id": 2,
+          "name": "Google I/O",
+          "date_created": "12th December 2015",
+          "date_modified": "12th December 2015",
+          "done": false
+        },
+        {
+          "item_id": 3,
+          "name": "WWDC",
+          "date_created": "12th December 2015",
+          "date_modified": "12th December 2015",
+          "done": false
+        }
+      ]
+    },
+  ];
   constructor() { }
 
   ngOnInit() {
