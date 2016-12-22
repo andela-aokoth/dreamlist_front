dc10c3ea71fa1c3bf3578870f77c1d04beef4981
Arnold Okoth <arnold.okoth@andela.com>
2016-12-22 16:24:20 +0300

    Add bucketlist component class

--- src/app/bucketlist/bucketlist.component.ts
+++ src/app/bucketlist/bucketlist.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
+import { BucketlistService } from './bucketlist.service';
+import { IBucketlist } from './bucketlist';
 
 @Component({
   selector: 'app-bucketlist',
@@ -6,131 +10,75 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bucketlist.component.css']
 })
 export class BucketlistComponent implements OnInit {
-  bucketlists: Array<any> = [
-    {
-      "bucketlist_id": 1,
-      "name": "Pluralsight Learning Paths",
-      "date_created": "12th December 2015",
-      "date_modified": "12th December 2015",
-      "description": "Getting the best out of my Pluralsight subscription",
-      "items": [
-        {
-          "item_id": 1,
-          "name": "JavaScript",
-          "date_created": "12th December 2015",
-          "date_modified": "12th December 2015",
-          "done": false
-        },
-        {
-          "item_id": 2,
-          "name": "AngularJS",
-          "date_created": "12th December 2015",
-          "date_modified": "12th December 2015",
-          "done": false
-        },
-        {
-          "item_id": 3,
-          "name": "Python",
-          "date_created": "12th December 2015",
-          "date_modified": "12th December 2015",
-          "done": false
-        }
-      ]
-    },
-    {
-      "bucketlist_id": 2,
-      "name": "Before Turning Twenty Five",
-      "date_created": "12th December 2015",
-      "date_modified": "12th December 2015",
-      "description": "Getting the best out of my Pluralsight subscription",
-      "items": [
-        {
-          "item_id": 1,
-          "name": "Tomorrowland",
-          "date_created": "12th December 2015",
-          "date_modified": "12th December 2015",
-          "done": false
-        },
-        {
-          "item_id": 2,
-          "name": "Google I/O",
-          "date_created": "12th December 2015",
-          "date_modified": "12th December 2015",
-          "done": false
-        },
-        {
-          "item_id": 3,
-          "name": "WWDC",
-          "date_created": "12th December 2015",
-          "date_modified": "12th December 2015",
-          "done": false
-        }
-      ]
-    },
-    {
-      "bucketlist_id": 3,
-      "name": "Before Turning Scary Thirty",
-      "date_created": "12th December 2015",
-      "date_modified": "12th December 2015",
-      "description": "Getting the best out of my Pluralsight subscription",
-      "items": [
-        {
-          "item_id": 1,
-          "name": "Tomorrowland",
-          "date_created": "12th December 2015",
-          "date_modified": "12th December 2015",
-          "done": false
-        },
-        {
-          "item_id": 2,
-          "name": "Google I/O",
-          "date_created": "12th December 2015",
-          "date_modified": "12th December 2015",
-          "done": false
-        },
-        {
-          "item_id": 3,
-          "name": "WWDC",
-          "date_created": "12th December 2015",
-          "date_modified": "12th December 2015",
-          "done": false
-        }
-      ]
-    },
-    {
-      "bucketlist_id": 4,
-      "name": "Before Turning Thirty Five",
-      "date_created": "12th December 2015",
-      "date_modified": "12th December 2015",
-      "description": "Getting the best out of my Pluralsight subscription",
-      "items": [
-        {
-          "item_id": 1,
-          "name": "Tomorrowland",
-          "date_created": "12th December 2015",
-          "date_modified": "12th December 2015",
-          "done": false
-        },
-        {
-          "item_id": 2,
-          "name": "Google I/O",
-          "date_created": "12th December 2015",
-          "date_modified": "12th December 2015",
-          "done": false
-        },
-        {
-          "item_id": 3,
-          "name": "WWDC",
-          "date_created": "12th December 2015",
-          "date_modified": "12th December 2015",
-          "done": false
-        }
-      ]
-    },
-  ];
-  constructor() { }
+  currentUser: string;
+  bucketlists: IBucketlist[];
+  errorMessage: string;
+
+  bucketlistId: number;
+  bucketlistName: string;
+  bucketlistDescription: string;
+
+  constructor(private _authService: AuthService, private _bucketlistService: BucketlistService, private _router: Router) {
+
+  }
+
+  getModalValues(id, name, desc) {
+    this.bucketlistId = id;
+    this.bucketlistName = name
+    this.bucketlistDescription = desc
+  }
 
   ngOnInit() {
+    if (this._authService.isLoggedIn()) {
+      this.currentUser = window.localStorage.getItem('username');
+      this._router.navigate(['/bucketlists']);
+    } else {
+      this._router.navigate(['/auth']);
+    }
+
+    this._bucketlistService.getBucketlists()
+      .subscribe(bucketlists => this.bucketlists = bucketlists,
+      error => this.errorMessage = <any>error);
+  }
+
+  createBucketlist(): void {
+    this._bucketlistService.createBucketlists(this.bucketlistName, this.bucketlistDescription).subscribe(
+      blist => {
+        console.log(blist);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    window.location.reload();
+  }
+
+  updateBucketlist(): void {
+    this._bucketlistService.updateBucketlist(this.bucketlistId, this.bucketlistName, this.bucketlistDescription).subscribe(
+      blist => {
+        console.log(blist);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    window.location.reload();
+  }
+
+  deleteBucketlist(): void {
+    this._bucketlistService.deleteBucketlist(this.bucketlistId).subscribe(
+      blist => {
+        console.log(blist);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    window.location.reload();
+  } 
+
+  logoutUser(): void {
+    this._authService.logoutUser();
   }
 
 }
